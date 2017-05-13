ed5cad84639f9595ee873575f3b9c7bec7e69231
GuilhermeCarrato <piocarrato1234@gmail.com>
2017-05-13 06:27:14 -0300

    ficheiro ingresso
    funcionando e comentado

---
+++ Ingresso/controle_sessao.py
@@ -0,0 +1,9 @@
+
+
+#ESSE MODULO FOI CRIADO PARA TESTES !!!!
+
+
+lista = [[1 ,[ 1 , "Alien" , "2 horas" , "18", "Ridley Scott", "tando faz", "cartas","Terror"],["12" , 1], 14], [2 ,[ 2 , "Guardiões da Galaxia" , "2 horas" , "18", "Ridley Scott", "tando faz", "cartas","Terror"],["12" , 2 ], 16],[3,[3,"machete","1:30","mendigo","lixeira","não devia mas ta em cartas","bosta"],["1/4",3],0000]]
+
+def listar_sessao():
+    return lista
