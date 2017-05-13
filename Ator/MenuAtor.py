d0ce824b0fdd80ce6d290c387daef2934f119b98
messias <messias@nmap.com.br>
2017-05-13 15:18:14 -0300

    Corrigido import de módulos

--- Ator/MenuAtor.py
+++ Ator/MenuAtor.py
@@ -1,5 +1,5 @@
-#import Controllerator
 from ator import Ator
+from ator import ControllerAtor
 
 def ShowMenu():
     print ("\n =========== MENU ator ============= ")
@@ -11,12 +11,23 @@ def ShowMenu():
     print ("0 - Voltar")
     opc = input("Digite o NÚMERO da opção: ")
 
-    if (opc == "1"):Ator.CadastrarAtor()
-    elif (opc == "2"):Ator.BuscarAtor()
-    elif (opc == "3"):Ator.BuscarAtores()
-    elif (opc == "4"):Ator.RemoverAtor()
-    elif (opc == "5"):Ator.RemoverTodosAtores()
-    elif (opc == "0"):MenuPrincipal()
+    if (opc == "1"):
+        Ator.CadastrarAtor()
+        ShowMenu()
+    elif (opc == "2"):
+        Ator.BuscarAtor()
+        ShowMenu()
+    elif (opc == "3"):
+        Ator.BuscarAtores()
+        ShowMenu()
+    elif (opc == "4"):
+        Ator.RemoverAtor()
+        ShowMenu()
+    elif (opc == "5"):
+        Ator.RemoverTodosAtores()
+        ShowMenu()
+    elif (opc == "0"):
+        return (opc)
 ##    ShowMenu()
 #Controllerator.IniciarAtores()
 ##ShowMenu()
