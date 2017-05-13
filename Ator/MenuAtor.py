bd2e5f599c92ef0d794e8fa2fbd1dbc7f8a50d51
messias <messias@nmap.com.br>
2017-05-12 21:44:27 -0300

    Correção do Import Módulo Menu Elenco

--- Ator/MenuAtor.py
+++ Ator/MenuAtor.py
@@ -1,9 +1,9 @@
-#import ControllerAtor
-import Ator
+#import Controllerator
+from ator import Ator
 
 def ShowMenu():
-    print ("\n =========== MENU ATOR ============= ")
-    print ("1 - Cadastrar Ator")
+    print ("\n =========== MENU ator ============= ")
+    print ("1 - Cadastrar ator")
     print ("2 - Buscar ator")
     print ("3 - Buscar todos atores")
     print ("4 - Remover ator")
@@ -17,6 +17,6 @@ def ShowMenu():
     elif (opc == "4"):Ator.RemoverAtor()
     elif (opc == "5"):Ator.RemoverTodosAtores()
     elif (opc == "0"):MenuPrincipal()
-    ShowMenu()
-#ControllerAtor.IniciarAtores()
-ShowMenu()
+##    ShowMenu()
+#Controllerator.IniciarAtores()
+##ShowMenu()
