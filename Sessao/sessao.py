f02de88d5da91dc28a6524bb5cd466c0714c9e76
messias <messias@nmap.com.br>
2017-05-14 09:37:14 -0300

    Adicionado verificação de filme não existe

--- Sessao/sessao.py
+++ Sessao/sessao.py
@@ -1,17 +1,19 @@
 from sessao import ControllerSessao
+from filme import Controller_filme
+from sala import Sala
 
 def CriarSessao():
     cod_sessao = int(input("Código Sessão: "))
     cod_filme = int(input("Código Filme: "))
-##    r = ControllerFilme.BuscarFilme(cod_filme)
-##    if (r == None):
-##        print ("Filme não existe")
-##        return (r)
+    r = Controller_filme.BuscarFilme(cod_filme)
+    if (r == None):
+       print ("Filme não existe")
+       return (r)
     cod_sala = int(input("Código Sala: "))
-##    r = ControllerSala.BuscaSala(cod_sala)
-##    if (r == None):
-##        print ("Sala não existe")
-##        
+    r = Sala.buscar_sala(cod_sala)
+    if (r == None):
+        print ("Sala não existe")
+        return (r)
     horario = input("Horário: ")
     ControllerSessao.CriarSessao(cod_sessao,cod_filme,cod_sala,horario)
 def RecuperarSessao():
