6de5137eec9b7ef12b4ef494d860c5bddae95bf1
messias <messias@nmap.com.br>
2017-05-14 13:22:25 -0300

    Teste sala

--- sala/TestSala.py
+++ sala/TestSala.py
@@ -6,47 +6,49 @@ class TestSala(unittest.TestCase):
         ControllerSala.RemoverTodasSalas()
 
     def test_sem_sessao(self):
-        s = ControllerAtor.ListarSala()
+        s = ControllerSala.ListarSala()
         self.assertEqual(0, len(s))
         
     def test_adicionar_sessao(self):
         ControllerSala.AdicionarSala(1,"livre")
         s = ControllerSala.BuscarSala(1)
-        
-        self.assertEqual(1,len(atores))
+        self.assertEqual(2,len(s))
         self.assertEqual(1, s[0])
         self.assertEqual("livre", s[1])
+        
+    def test_buscar_sessao(self):
+        ControllerSala.AdicionarSala(1,"livre")
+        
+        a = ControllerSala.BuscarSala(1)
+        self.assertEqual(1,a[0])
+        self.assertEqual("livre",a[1])
 
         
-    def test_buscar_ator(self):
-        ControllerSala.AdicionarSala(1,"
-        ControllerAtor.CadastrarAtor(1,"Messias","Brasileiro",21)
-        a = ControllerAtor.BuscarAtor(1)
-        self.assertEqual(1,a[0])
-        self.assertEqual("Messias",a[1])
-        self.assertEqual("Brasileiro",a[2])
-        self.assertEqual(21,a[3])
-    def test_buscar_atores(self):
-        ControllerAtor.CadastrarAtor(1,"Messias","Brasileiro",21)
-        ControllerAtor.CadastrarAtor(2,"Teste","Brasileiro",1)
-        a = ControllerAtor.BuscarAtores()
+    def test_buscar_sessoes(self):
+        ControllerSala.AdicionarSala(1,"livre")
+        ControllerSala.AdicionarSala(2,"ocupado")
+
+        a = ControllerSala.ListarSala()
+        a = len(a)
+        self.assertEqual(2,a)
+
         
-    def test_remover_ator(self):
-        ControllerAtor.CadastrarAtor("1","Messias","Brasileiro",21)
-        ControllerAtor.RemoverAtor(1)
-        a = ControllerAtor.BuscarAtor(1)
+    def test_remover(self):
+        ControllerSala.AdicionarSala(1,"livre")
+        ControllerSala.RemoverSala(1)
+        a = ControllerSala.BuscarSala(1)
         self.assertIsNone(a)
         
-    def test_remover_todos_atores(self):
-        ControllerAtor.CadastrarAtor(1,"Messias","Brasileiro",21)
-        ControllerAtor.CadastrarAtor(2,"Teste","Brasileiro",1)
-        a = ControllerAtor.RemoverTodosAtores()
+    def test_remover_todos(self):
+        ControllerSala.AdicionarSala(1,"livre")
+        ControllerSala.AdicionarSala(2,"ocupado")
+        a = ControllerSala.RemoverTodasSalas()
         self.assertEqual([], a)
         
-    def test_iniciar_atores(self):
-        ControllerAtor.IniciarAtores()
-        atores = ControllerAtor.BuscarAtores()
-        self.assertEqual(3, len(atores))
+    def test_iniciar_sala(self):
+        ControllerSala.IniciarSala()
+        sala = ControllerSala.ListarSala()
+        self.assertEqual(2, len(sala))
 
             
 if __name__ == '__main__':
