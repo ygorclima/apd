6daab22cc5c0a20947e5172e07e5b26a24d1d1bd
messias <messias@nmap.com.br>
2017-05-14 02:03:26 -0300

    Correção dos teste

--- Ingresso/TestIngresso.py
+++ Ingresso/TestIngresso.py
@@ -7,30 +7,27 @@ class TestElenco(unittest.TestCase):
 
     def test_sem_ingresso(self):
         ingresso = ControllerIngresso.ListarIngresso()
-        print ("aqui",ingresso)
-        self.assertEqual(0,len(ingresso))
+        self.assertIsNone(ingresso)
                          
     def test_buscar_ingresso(self):
         ControllerIngresso.Ingresso(1)
         e = ControllerIngresso.BuscaIngresso(1)
         self.assertEqual(1,e[0])
+        self.assertEqual("inteiro",e[1])
                          
     def test_remover(self):
         ControllerIngresso.Ingresso(1)
-        ControllerjIngresso.RemoverIngresso(1)
-        e = ControllerIngresso.BuscarIngresso(1)
+        ControllerIngresso.RemoverIngresso(1)
+        e = ControllerIngresso.BuscaIngresso(1)
         self.assertIsNone(e)
+        
 
     def test_remover_todos(self):
         ControllerIngresso.Ingresso(1)
         ControllerIngresso.Ingresso(1)
         e = ControllerIngresso.RemoverTodosIngresso()
         self.assertEqual([],e)
-        
-    def test_iniciar_elenco(self):
-        ControllerIngresso.IniciarIngresso()
-        e = ControllerIngresso.BuscarTodosIngresso()
-        self.assertEqual(2, len(e))
+
         
 if __name__ == '__main__':
     unittest.main(exit=False)
