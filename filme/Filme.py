411aef12045b2c66d5ed394330916e72bff70de8
messias <messias@nmap.com.br>
2017-05-14 10:32:10 -0300

    Funcionalidades das funções

--- filme/Filme.py
+++ filme/Filme.py
@@ -1,11 +1,22 @@
+from filme import ControllerFilme
+
 def CadastrarFilme():
-    pass
+    cod_filme = int(input("Código filme: "))
+    titulo = input("Título: ")
+    duracao = input("Duração: ")
+    classificacao = input("Classificação: ")
+    diretor = input("Diretor: ")
+    distribuidora = input("Distribuidora: ")
+    status = input("Status: ")
+    genero = input("Gênero: ")
+    ControllerFilme.CadastrarFilme(cod_filme,titulo,duracao,classificacao,diretor,distribuidora,status,genero)
 
 def BuscarFilme():
-    pass
-
+    cod_filme = int(input("Código Filme: "))
+    ControllerFilme.BuscarFilme(cod_filme)
+    
 def ListarFilmes():
-    pass
+    ControllerFilme.ListarFilmes()
 
 def RemoverTodosFilmes():
-    pass
+    ControllerFilme.RemoverTodosFilmes()
